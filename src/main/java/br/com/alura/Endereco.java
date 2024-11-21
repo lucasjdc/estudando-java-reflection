100f253f77427b84febdb5291e68369eabefc343
lucasjdc <lucasjpdc@gmail.com>
2024-11-21 17:58:42 -0300

    Teste shouldNotTransform

---
+++ src/main/java/br/com/alura/Endereco.java
@@ -0,0 +1,13 @@
+package br.com.alura;
+
+public class Endereco {
+
+    public Endereco(String rua, Integer numero) {
+        this.rua = rua;
+        this.numero = numero;
+    }
+
+    private String rua;
+    private Integer numero;
+
+}
