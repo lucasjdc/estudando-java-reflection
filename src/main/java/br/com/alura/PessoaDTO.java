8fc6f65722ca3ef437ea88ecce7d8087de2e8c43
lucasjdc <lucasjpdc@gmail.com>
2024-11-21 17:37:53 -0300

    Teste shouldTransform

--- src/main/java/br/com/alura/PessoaDTO.java
+++ src/main/java/br/com/alura/PessoaDTO.java
@@ -5,8 +5,11 @@ public class PessoaDTO {
     private String nome;
     private String cpf;
 
-    public PessoaDTO(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
     }
 }
